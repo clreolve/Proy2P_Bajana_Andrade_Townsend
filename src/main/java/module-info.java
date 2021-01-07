3e58497f0ce168eb5dc616697baf06f3a030de99
Neoterux <labfernandez2014@gmail.com>
2021-01-07 14:34:33 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.neoterux.proyecto2p {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.neoterux.proyecto2p to javafx.fxml;
+    exports com.neoterux.proyecto2p;
+}
