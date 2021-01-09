7c111f8ed1367d481a4febe5d0a54dc7c55c47f9
Neoterux <labfernandez2014@gmail.com>
2021-01-09 18:45:23 -0500

    Added main view

---
+++ src/main/java/com/neoterux/proyecto2p/ui/controllers/MainController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.neoterux.proyecto2p.ui.controllers;
+
+import com.neoterux.proyecto2p.App;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+/**
+ *
+ * @author neoterux
+ */
+public class MainController implements Initializable{
+    
+    @FXML
+    private GridPane root;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates
+        
+    }
+    
+}
