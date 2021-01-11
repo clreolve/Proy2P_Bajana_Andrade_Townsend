6a29e6dd782311747f3bccdd32c42289395bb213
Neoterux <labfernandez2014@gmail.com>
2021-01-10 21:39:31 -0500

    Added Log4J2 dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.neoterux.proyecto2p {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.logging.log4j;
 
-    opens com.neoterux.proyecto2p to javafx.fxml;
-    opens com.neoterux.proyecto2p.ui.controllers to javafx.fxml;
+    opens com.neoterux.proyecto2p to javafx.fxml, org.apache.logging.log4j;
+    opens com.neoterux.proyecto2p.ui.controllers to javafx.fxml, org.apache.logging.log4j;
     exports com.neoterux.proyecto2p;
 }
