6c691602d62eff4d7313570a891ea4b8c7b2d738
Neoterux <labfernandez2014@gmail.com>
2021-01-11 22:10:17 -0500

    Added images paths

--- src/main/java/com/neoterux/proyecto2p/App.java
+++ src/main/java/com/neoterux/proyecto2p/App.java
@@ -7,6 +7,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * <h1>JavaFX App</h1><p>
@@ -17,6 +23,24 @@ import java.io.IOException;
  * @version 0.0.1
  */
 public class App extends Application {
+    /*
+    Images APIs 
+    
+    iso_code : ex. Ecuador -> ec / EC
+    size : depends of api regular is width x height. but also can be only one parameter
+            ex. ' 64x64' or '64'
+    
+    site: https://flagcdn.com/[size{width x height}]/[iso_code].png
+    
+    */
+    
+    public static volatile Path FLAGS_PATH = Paths.get("imgs", "flags");
+    public static volatile Path COUNTRIES_PATH = Paths.get("imgs", "countries");
+    
+    
+    private static Logger logger = LogManager.getLogger(App.class);
+    
+    public static ThreadGroup appThreadGroup = new ThreadGroup("Proyect2"); 
     
     private static int WIDTH = 640;
     private static int HEIGHT = 400;
@@ -25,7 +49,18 @@ public class App extends Application {
     public static Stage mainStage;
 
     @Override
+    public void init() throws Exception {
+        super.init(); //To change body of generated methods, choose Tools | Templates.
+        
+        FLAGS_PATH.toFile().mkdirs();
+        COUNTRIES_PATH.toFile().mkdirs();
+    }
+    
+    
+
+    @Override
     public void start(Stage stage) throws IOException {
+        logger.info("Starting application");
         /*
             BASE FX APPLICATION
         
@@ -42,17 +77,38 @@ public class App extends Application {
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    @Override
+    public void stop() throws Exception {
+        logger.info("exitting...");
+        
+        System.exit(0);//close all posible threads
+    }
+    
+    
+
+    public static void setRoot(String fxml, String title) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        logger.debug("loading fxml at: " + fxml); 
+        var fxml_url = App.class.getResource(fxml + ".fxml"); 
+        FXMLLoader fxmlLoader = new FXMLLoader(fxml_url);
+        logger.debug("fxml url: " + fxml_url);
         return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+    
+    public synchronized static URL resourceFrom(Class from, String relativePath) {
+        return from.getResource(relativePath);
+//        try { 
+//            var res = Paths.get(from.getResource(relativePath).toURI());
+//        }catch(URISyntaxException use) {
+//            System.out.println("Error when loading resource: " + use.getMessage());
+//        }
     }
 
 }
