0589c9d60e57e95739318a012c7f9edababa4530
Neoterux <labfernandez2014@gmail.com>
2021-01-16 20:03:35 -0500

    Added get method to read globales.csv file

--- src/main/java/com/neoterux/proyecto2p/model/Country.java
+++ src/main/java/com/neoterux/proyecto2p/model/Country.java
@@ -10,8 +10,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,10 +38,20 @@ public class Country implements Comparable {
     
     
     private String name;
+    private int cases;
+    private int totalDeaths;
     
     public Country (String name){
         this.name = name;
     }
+    
+    public Country (String name, int cases, int totalDeaths) {
+        this(name);
+        this.cases = cases;
+        this.totalDeaths = totalDeaths;
+    }
+    
+    
         
 
     @Override
@@ -56,5 +65,93 @@ public class Country implements Comparable {
     @Override 
     public String toString() {
         return this.getName();
+    }
+    
+    /**
+     * Carga los datos del archivo globales.csv
+     * 
+     * @return lista con los datos parseados del csv a objeto Country. 
+     */
+    public static List<Country> get() {
+        if (countries == null){
+            var con = Continent.loadContinents();
+            
+            // This is for complete country data 
+            var cmap = new HashMap<String, Country>();
+            // this is the list with full data of countries
+            var lst = new ArrayList<Country>();
+            con.stream()
+                    .map(it -> it.getCountries())
+                    .forEach(it ->{ // this is a list of countries by continent
+                        it.forEach(country ->{
+                            lst.add(country);
+                            cmap.put(country.name, country);
+                        });
+                    });
+            
+            // Now lets complete countries data
+            var file = Paths.get(App.FILES_PATH.toString(), "globales.csv").toFile();
+            // Read with a buffer of 10 MiB
+            try(var reader = new BufferedReader(new FileReader(file), 10240)){
+                
+                reader.lines()
+                        .skip(1)
+                        .map(it -> it.split("[|]"))
+                        .forEach(data ->{
+                            /*
+                            * data[0]: Country name
+                            * data[1]: Total cases
+                            * data[2]: Total deaths
+                            */
+                            // This can cause stackOverflow
+                            // but with the max countries number is 196
+                            // I think that this doesnt represent high memory usage
+                            var c = cmap.get(data[0]);
+                            c.cases = Integer.parseInt(data[1]);
+                            c.totalDeaths = Integer.parseInt(data[2]);
+                            
+                        });
+                
+            }catch (FileNotFoundException fnf){
+                logger.error("globales.csv not found in data folder");
+            }catch (IOException ioe) {
+                logger.error("An IOException ocurred when trying to read globales.csv file ", ioe); 
+            }
+            catch (Exception e) {
+                logger.error("Unknown exception when trying to read globale.csv file", e);
+            }
+            countries = lst; 
+        }
+        
+        return countries;
+    }
+
+
+    /**
+     * @return the cases
+     */
+    public int getCases() {
+        return cases;
+    }
+
+    /**
+     * @param cases the cases to set
+     */
+    public void setCases(int cases) {
+        this.cases = cases;
+    }
+
+    /**
+     * @return the totalDeaths
+     */
+    public int getTotalDeaths() {
+        return totalDeaths;
+    }
+
+    /**
+     * @param totalDeaths the totalDeaths to set
+     */
+    public void setTotalDeaths(int totalDeaths) {
+        this.totalDeaths = totalDeaths;
     }
 }
